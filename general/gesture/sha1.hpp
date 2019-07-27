572d901632d1338686570de19d146d735ebc5e17
aplqo <aplqo@outlook.com>
2019-07-27 10:13:24 +0800

    Format document

--- general/gesture/sha1.hpp
+++ general/gesture/sha1.hpp
@@ -6,15 +6,16 @@ typedef unsigned long long bit64;
 namespace hash
 {
     class sha1
-	{
-		public:
-		sha1();
-		bool operator== (const sha1 &a);
-		void getnum(bit8 *a,bit64 length);
-		bit32 num[5];
-		private:
-		const bit32 k[4]= {0x5A827999,0x6ED9EBA1,0x8F1BBCDC,0xCA62C1D6};
-		bit32 f(const bit32 &b,const bit32 &c,const bit32 &d,unsigned int e);
-	};
+    {
+    public:
+        sha1();
+        bool operator==(const sha1& a);
+        void getnum(bit8* a, bit64 length);
+        bit32 num[5];
+
+    private:
+        const bit32 k[4] = { 0x5A827999, 0x6ED9EBA1, 0x8F1BBCDC, 0xCA62C1D6 };
+        bit32 f(const bit32& b, const bit32& c, const bit32& d, unsigned int e);
+    };
 }
 #endif
