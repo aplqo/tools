61c4091de69218c7b35a2d033d61d840b8babd38
aplqo <aplqo@outlook.com>
2019-08-15 21:21:16 +0800

    Add files for ir-eeprom

---
+++ general/ir-eeprom/ir-eeprom.cpp
@@ -0,0 +1,15 @@
+﻿#include <fstream>
+#include<iostream>
+
+using std::cout;
+using std::ofstream;
+using std::endl;
+
+int main(int argc, char* argv[])
+{
+    if (argc < 3)
+    {
+        cout << "Too few arguments" << endl;
+        return -1;
+	}
+}
